b2476f3b3cec0ad1e2683e1dee2a6f34d99b2a0c
K Harsh <harshnaidu2u@gmail.com>
2023-09-14 23:04:35 +0530

    server actions (buttons)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ function getTodos(){
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({where: {id}, data: {complete}} )
+  console.log(id, complete);
+  
+}
+
 export default async function Home() {
   const todos = await getTodos();
   
@@ -21,7 +29,7 @@ export default async function Home() {
     </header>
     <ul className="pl-4">
       {todos.map(todo => 
-        <TodoItem key={todo.id} {...todo} />
+        <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
       )}
     </ul>
     </>
